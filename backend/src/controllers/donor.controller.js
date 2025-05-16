0cc7e25401f5def67fe994f20f67ad504a77ec30
Matthias Gousseau <matthias.gousseau@hotmail.com>
2025-05-16 19:13:23 +0200

    suppression exportDonor.controller

--- backend/src/controllers/donor.controller.js
+++ backend/src/controllers/donor.controller.js
@@ -1,5 +1,6 @@
 // donor.controller.js
 const Donor = require("../models/donor.model")
+const exportDonorService = require('../services/exportDonor.service');
 
 exports.getDonors = async (req, res) => {
 	try {
@@ -118,3 +119,39 @@ exports.getDonorByEmail = async (req, res) => {
 		res.status(500).send("Erreur lors de la récupération du donateur")
 	}
 }
+
+exports.exportToExcel = async (req, res) => {
+  try {
+    // Récupérer tous les doneurs
+    const donors = await Donor.getAll();
+    
+    // Générer le fichier Excel
+    const buffer = await exportDonorService.generateExcel(donors);
+    
+    // Envoyer le fichier
+    res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    res.setHeader('Content-Disposition', 'attachment; filename="liste des donateurs.xlsx"');
+    res.send(buffer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erreur lors de l\'exportation vers Excel');
+  }
+};
+
+exports.exportToPdf = async (req, res) => {
+  try {
+    // Récupérer tous les doneurs
+    const donors = await Donor.getAll();
+    
+    // Générer le PDF
+    const buffer = await exportDonorService.generatePDF(donors);
+    
+    // Envoyer le PDF
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'attachment; filename="liste des donateurs.pdf"');
+    res.send(buffer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erreur lors de l\'exportation vers PDF');
+  }
+};
