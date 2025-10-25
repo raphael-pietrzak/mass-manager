152f9dd36159dcb9055083f88e6de43eb993ad07
Matthias Gousseau <matthias.gousseau@hotmail.com>
2025-10-25 14:53:33 +0200

    modifs cron

--- backend/src/services/cron/cron.service.js
+++ backend/src/services/cron/cron.service.js
@@ -1,5 +1,6 @@
-import cron from "node-cron";
-import axios from "axios";
+const cron = require("node-cron")
+const axios = require("axios")
+const { log } = require("../logger")
 
 // toutes les 30s ==> "*/30 * * * * *"
 // toutes les minutes "* * * * *"
@@ -9,20 +10,34 @@ import axios from "axios";
 
 // Marquer les messes et intentions terminées en completed
 // Vérification effectuée tous les dimanche à minuit
-cron.schedule("0 0 0 * * 0", async () => {
-	axios.patch("http://localhost:3001/api/cron/updateMasses")
+cron.schedule("0 0 0 * * SUN", async () => {
+	try {
+		await axios.patch("http://localhost:3001/api/cron/updateMasses")
+	} catch (err) {
+		console.error("Erreur CRON mensuel :", err.message)
+		log(err.message, "ERROR")
+	}
 })
-
 
 // 1er janvier à midi → messes annuelles sans limite de date ou d'occurence
 // on affecte la messe pour l'année suivante
 cron.schedule("0 12 1 1 *", async () => {
-  axios.post("http://localhost:3001/api/cron/assignAnnualMassesWithNoEnd")
+	try {
+		await axios.post("http://localhost:3001/api/cron/assignAnnualMassesWithNoEnd")
+	} catch (err) {
+		console.error("Erreur CRON mensuel :", err.message)
+		log(err.message, "ERROR")
+	}
 })
 
 // CRON : 1er jour de chaque mois à midi → messes mensuelles
 // on affecte les messes de ce mois pour 12 mois après
 cron.schedule("0 12 1 * *", async () => {
-  axios.post("http://localhost:3001/api/cron/assignMonthlyMassesWithNoEnd")
-  axios.post("http://localhost:3001/api/cron/assignMonthlyRelativePositionMassesWithNoEnd")
+	try {
+		await axios.post("http://localhost:3001/api/cron/assignMonthlyMassesWithNoEnd")
+		await axios.post("http://localhost:3001/api/cron/assignMonthlyRelativePositionMassesWithNoEnd")
+	} catch (err) {
+		console.error("Erreur CRON mensuel :", err.message)
+		log(err.message, "ERROR")
+	}
 })
